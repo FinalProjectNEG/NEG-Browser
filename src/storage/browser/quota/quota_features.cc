68c58c2c4002267b1706b1d71bca674e21784607
edenda2 <edenda2@ac.sce.ac.il>
2021-02-19 22:15:02 +0200

    upload more files

---
+++ src/storage/browser/quota/quota_features.cc
@@ -0,0 +1,16 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "storage/browser/quota/quota_features.h"
+
+namespace storage {
+
+namespace features {
+
+// Enables Storage Pressure Event.
+const base::Feature kStoragePressureEvent{"StoragePressureEvent",
+                                          base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace features
+}  // namespace storage
