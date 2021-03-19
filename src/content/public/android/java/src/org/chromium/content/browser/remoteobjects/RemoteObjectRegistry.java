43a2edd4a4e5795c751987271fefb391ec261a51
edenda2 <edenda2@ac.sce.ac.il>
2021-03-19 03:59:02 +0200

    update passwords and logo more files

---
+++ src/content/public/android/java/src/org/chromium/content/browser/remoteobjects/RemoteObjectRegistry.java
@@ -0,0 +1,96 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content.browser.remoteobjects;
+
+import android.util.SparseArray;
+
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Owns a set of objects on behalf of RemoteObjectHost's client.
+ *
+ * These objects could contain references which would keep the WebContents alive
+ * longer than expected, and so must not be held alive by any other GC root.
+ */
+final class RemoteObjectRegistry implements RemoteObjectImpl.ObjectIdAllocator {
+    private final Set<? super RemoteObjectRegistry> mRetainingSet;
+
+    private static class Entry {
+        Entry(int id, Object object) {
+            this.id = id;
+            this.object = object;
+        }
+
+        // The ID assigned to the object, which can be used by the renderer
+        // to refer to it.
+        public final int id;
+
+        // The injected object itself
+        public final Object object;
+
+        // The number of outstanding references to this object.
+        // This includes:
+        //   * wrapper objects in the renderer (removed when it closes the pipe)
+        //   * names assigned to the object by developer Java code
+        public int referenceCount = 1;
+    }
+
+    private final SparseArray<Entry> mEntriesById = new SparseArray<>();
+    private final Map<Object, Entry> mEntriesByObject = new IdentityHashMap<>();
+    private int mNextId;
+
+    RemoteObjectRegistry(Set<? super RemoteObjectRegistry> retainingSet) {
+        retainingSet.add(this);
+        mRetainingSet = retainingSet;
+    }
+
+    public void close() {
+        boolean removed = mRetainingSet.remove(this);
+        assert removed;
+    }
+
+    @Override
+    public synchronized int getObjectId(Object object) {
+        Entry entry = mEntriesByObject.get(object);
+        if (entry != null) {
+            entry.referenceCount++;
+            return entry.id;
+        }
+
+        int newId = mNextId++;
+        assert newId >= 0;
+        entry = new Entry(newId, object);
+        mEntriesById.put(newId, entry);
+        mEntriesByObject.put(object, entry);
+        return newId;
+    }
+
+    @Override
+    public synchronized Object getObjectById(int id) {
+        Entry entry = mEntriesById.get(id);
+        return entry != null ? entry.object : null;
+    }
+
+    public synchronized void refObjectById(int id) {
+        Entry entry = mEntriesById.get(id);
+        if (entry == null) return;
+
+        entry.referenceCount++;
+    }
+
+    public synchronized void unrefObjectById(int id) {
+        Entry entry = mEntriesById.get(id);
+        if (entry == null) return;
+
+        entry.referenceCount--;
+        assert entry.referenceCount >= 0;
+        if (entry.referenceCount > 0) return;
+
+        mEntriesById.remove(entry.id);
+        mEntriesByObject.remove(entry.object);
+    }
+}
