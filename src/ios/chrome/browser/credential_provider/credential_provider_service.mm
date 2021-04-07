463c7a8afa51f83895b8e5f1ebdb6534c4018e6b
sapirsa3 <sapirsa3@ac.sce.ac.il>
2021-04-08 00:27:39 +0300

    print passwords from DB

---
+++ src/ios/chrome/browser/credential_provider/credential_provider_service.mm
@@ -0,0 +1,333 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/credential_provider/credential_provider_service.h"
+
+#import <AuthenticationServices/AuthenticationServices.h>
+
+#include "base/check.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/notreached.h"
+#include "base/scoped_observer.h"
+#include "base/strings/sys_string_conversions.h"
+#include "base/threading/sequenced_task_runner_handle.h"
+#include "build/build_config.h"
+#include "components/password_manager/core/browser/android_affiliation/affiliated_match_helper.h"
+#include "components/password_manager/core/browser/android_affiliation/affiliation_utils.h"
+#include "components/password_manager/core/browser/android_affiliation/android_affiliation_service.h"
+#include "components/password_manager/core/browser/password_manager_util.h"
+#include "components/password_manager/core/browser/password_store.h"
+#include "components/password_manager/core/browser/password_store_change.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/sync/driver/sync_service.h"
+#include "ios/chrome/browser/credential_provider/archivable_credential+password_form.h"
+#import "ios/chrome/browser/credential_provider/credential_provider_util.h"
+#include "ios/chrome/common/app_group/app_group_constants.h"
+#import "ios/chrome/common/credential_provider/archivable_credential.h"
+#import "ios/chrome/common/credential_provider/archivable_credential_store.h"
+#import "ios/chrome/common/credential_provider/as_password_credential_identity+credential.h"
+#import "ios/chrome/common/credential_provider/constants.h"
+#import "ios/public/provider/chrome/browser/signin/chrome_identity.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace {
+
+using autofill::PasswordForm;
+using password_manager::AffiliatedMatchHelper;
+using password_manager::PasswordStore;
+using password_manager::PasswordStoreChange;
+using password_manager::PasswordStoreChangeList;
+using password_manager::AndroidAffiliationService;
+
+// ASCredentialIdentityStoreError enum to report UMA metrics. Must be in sync
+// with iOSCredentialIdentityStoreErrorForReporting in
+// tools/metrics/histograms/enums.xml.
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class CredentialIdentityStoreErrorForReporting {
+  kUnknownError,
+  kInternal,
+  kDisabled,
+  kBusy,
+  kMaxValue = kBusy
+};
+
+// Converts a UIKit interface style to an interface style for reporting.
+CredentialIdentityStoreErrorForReporting
+ErrorForReportingForASCredentialIdentityStoreErrorCode(
+    ASCredentialIdentityStoreErrorCode errorCode) {
+  switch (errorCode) {
+    case ASCredentialIdentityStoreErrorCodeInternalError:
+      return CredentialIdentityStoreErrorForReporting::kInternal;
+    case ASCredentialIdentityStoreErrorCodeStoreDisabled:
+      return CredentialIdentityStoreErrorForReporting::kDisabled;
+    case ASCredentialIdentityStoreErrorCodeStoreBusy:
+      return CredentialIdentityStoreErrorForReporting::kBusy;
+  }
+  return CredentialIdentityStoreErrorForReporting::kUnknownError;
+}
+
+BOOL ShouldSyncAllCredentials() {
+  NSUserDefaults* user_defaults = [NSUserDefaults standardUserDefaults];
+  DCHECK(user_defaults);
+  return ![user_defaults
+      boolForKey:kUserDefaultsCredentialProviderFirstTimeSyncCompleted];
+}
+
+BOOL ShouldSyncASIdentityStore() {
+  NSUserDefaults* user_defaults = [NSUserDefaults standardUserDefaults];
+  DCHECK(user_defaults);
+  BOOL isIdentityStoreSynced = [user_defaults
+      boolForKey:kUserDefaultsCredentialProviderASIdentityStoreSyncCompleted];
+  BOOL areCredentialsSynced = [user_defaults
+      boolForKey:kUserDefaultsCredentialProviderFirstTimeSyncCompleted];
+  return !isIdentityStoreSynced && areCredentialsSynced;
+}
+
+void SyncASIdentityStore(ArchivableCredentialStore* credential_store) {
+  auto stateCompletion = ^(ASCredentialIdentityStoreState* state) {
+#if !defined(NDEBUG)
+    dispatch_assert_queue_not(dispatch_get_main_queue());
+#endif  // !defined(NDEBUG)
+    if (state.enabled) {
+      NSArray<id<Credential>>* credentials = credential_store.credentials;
+      NSMutableArray<ASPasswordCredentialIdentity*>* storeIdentities =
+          [NSMutableArray arrayWithCapacity:credentials.count];
+      for (id<Credential> credential in credentials) {
+        [storeIdentities addObject:[[ASPasswordCredentialIdentity alloc]
+                                       initWithCredential:credential]];
+      }
+      auto replaceCompletion = ^(BOOL success, NSError* error) {
+        // Sometimes ASCredentialIdentityStore fails. Log this to measure the
+        // impact of these failures and move on.
+        if (!success) {
+          ASCredentialIdentityStoreErrorCode code =
+              static_cast<ASCredentialIdentityStoreErrorCode>(error.code);
+          CredentialIdentityStoreErrorForReporting errorForReporting =
+              ErrorForReportingForASCredentialIdentityStoreErrorCode(code);
+          base::UmaHistogramEnumeration(
+              "IOS.CredentialExtension.Service.Error."
+              "ReplaceCredentialIdentitiesWithIdentities",
+              errorForReporting);
+        }
+        NSUserDefaults* user_defaults = [NSUserDefaults standardUserDefaults];
+        NSString* key =
+            kUserDefaultsCredentialProviderASIdentityStoreSyncCompleted;
+        [user_defaults setBool:success forKey:key];
+      };
+      [ASCredentialIdentityStore.sharedStore
+          replaceCredentialIdentitiesWithIdentities:storeIdentities
+                                         completion:replaceCompletion];
+    }
+  };
+  [ASCredentialIdentityStore.sharedStore
+      getCredentialIdentityStoreStateWithCompletion:stateCompletion];
+}
+
+}  // namespace
+
+CredentialProviderService::CredentialProviderService(
+    scoped_refptr<PasswordStore> password_store,
+    AuthenticationService* authentication_service,
+    ArchivableCredentialStore* credential_store,
+    signin::IdentityManager* identity_manager,
+    syncer::SyncService* sync_service)
+    : password_store_(password_store),
+      authentication_service_(authentication_service),
+      identity_manager_(identity_manager),
+      sync_service_(sync_service),
+      archivable_credential_store_(credential_store) {
+  DCHECK(password_store_);
+  password_store_->AddObserver(this);
+
+  DCHECK(authentication_service_);
+  UpdateAccountValidationId();
+
+  if (identity_manager_) {
+    identity_manager_->AddObserver(this);
+  }
+
+  bool is_sync_active = false;
+  if (sync_service_) {
+    sync_service_->AddObserver(this);
+    is_sync_active = sync_service_->IsSyncFeatureActive();
+  }
+
+  // If Sync is active, wait for the configuration to finish before syncing.
+  // This will wait for affiliated_match_helper to be available.
+  if (!is_sync_active) {
+    RequestSyncAllCredentialsIfNeeded();
+  }
+}
+
+CredentialProviderService::~CredentialProviderService() {}
+
+void CredentialProviderService::Shutdown() {
+  password_store_->RemoveObserver(this);
+  if (identity_manager_) {
+    identity_manager_->RemoveObserver(this);
+  }
+  if (sync_service_) {
+    sync_service_->RemoveObserver(this);
+  }
+}
+
+void CredentialProviderService::RequestSyncAllCredentials() {
+  UpdateAccountValidationId();
+  password_store_->GetAutofillableLogins(this);
+}
+
+void CredentialProviderService::RequestSyncAllCredentialsIfNeeded() {
+  if (ShouldSyncASIdentityStore()) {
+    SyncASIdentityStore(archivable_credential_store_);
+  }
+  if (ShouldSyncAllCredentials()) {
+    RequestSyncAllCredentials();
+  }
+}
+
+void CredentialProviderService::SyncAllCredentials(
+    std::vector<std::unique_ptr<PasswordForm>> forms) {
+  [archivable_credential_store_ removeAllCredentials];
+  AddCredentials(std::move(forms));
+  SyncStore(true);
+}
+
+void CredentialProviderService::SyncStore(bool set_first_time_sync_flag) {
+  __weak ArchivableCredentialStore* weak_archivable_credential_store =
+      archivable_credential_store_;
+  [archivable_credential_store_ saveDataWithCompletion:^(NSError* error) {
+    if (error) {
+      return;
+    }
+    if (set_first_time_sync_flag) {
+      NSUserDefaults* user_defaults = [NSUserDefaults standardUserDefaults];
+      for (NSString* key in UnusedUserDefaultsCredentialProviderKeys()) {
+        [user_defaults removeObjectForKey:key];
+      }
+      NSString* key = kUserDefaultsCredentialProviderFirstTimeSyncCompleted;
+      [user_defaults setBool:YES forKey:key];
+    }
+    if (weak_archivable_credential_store) {
+      SyncASIdentityStore(weak_archivable_credential_store);
+    }
+  }];
+}
+
+void CredentialProviderService::AddCredentials(
+    std::vector<std::unique_ptr<PasswordForm>> forms) {
+  for (const auto& form : forms) {
+    ArchivableCredential* credential = [[ArchivableCredential alloc]
+        initWithPasswordForm:*form
+                     favicon:nil
+        validationIdentifier:account_validation_id_];
+    DCHECK(credential);
+    [archivable_credential_store_ addCredential:credential];
+  }
+}
+
+void CredentialProviderService::RemoveCredentials(
+    std::vector<std::unique_ptr<PasswordForm>> forms) {
+  for (const auto& form : forms) {
+    NSString* recordID = RecordIdentifierForPasswordForm(*form);
+    DCHECK(recordID);
+    [archivable_credential_store_
+        removeCredentialWithRecordIdentifier:recordID];
+  }
+}
+
+void CredentialProviderService::UpdateAccountValidationId() {
+  if (authentication_service_->IsAuthenticatedIdentityManaged()) {
+    account_validation_id_ =
+        authentication_service_->GetAuthenticatedIdentity().gaiaID;
+  } else {
+    account_validation_id_ = nil;
+  }
+  [app_group::GetGroupUserDefaults()
+      setObject:account_validation_id_
+         forKey:AppGroupUserDefaultsCredentialProviderManagedUserID()];
+}
+
+void CredentialProviderService::OnGetPasswordStoreResults(
+    std::vector<std::unique_ptr<PasswordForm>> results) {
+  auto callback = base::BindOnce(&CredentialProviderService::SyncAllCredentials,
+                                 weak_factory_.GetWeakPtr());
+  AffiliatedMatchHelper* matcher = password_store_->affiliated_match_helper();
+  if (matcher) {
+    matcher->InjectAffiliationAndBrandingInformation(
+        std::move(results),
+        AndroidAffiliationService::StrategyOnCacheMiss::FETCH_OVER_NETWORK,
+        std::move(callback));
+  } else {
+    std::move(callback).Run(std::move(results));
+  }
+}
+
+void CredentialProviderService::OnPrimaryAccountSet(
+    const CoreAccountInfo& primary_account_info) {
+  RequestSyncAllCredentials();
+}
+
+void CredentialProviderService::OnPrimaryAccountCleared(
+    const CoreAccountInfo& previous_primary_account_info) {
+  RequestSyncAllCredentials();
+}
+
+void CredentialProviderService::OnLoginsChanged(
+    const PasswordStoreChangeList& changes) {
+  std::vector<std::unique_ptr<PasswordForm>> forms_to_add;
+  std::vector<std::unique_ptr<PasswordForm>> forms_to_remove;
+  for (const PasswordStoreChange& change : changes) {
+    if (change.form().blocked_by_user) {
+      continue;
+    }
+    switch (change.type()) {
+      case PasswordStoreChange::ADD:
+        forms_to_add.push_back(std::make_unique<PasswordForm>(change.form()));
+        break;
+      case PasswordStoreChange::UPDATE:
+        forms_to_remove.push_back(
+            std::make_unique<PasswordForm>(change.form()));
+        forms_to_add.push_back(std::make_unique<PasswordForm>(change.form()));
+        break;
+      case PasswordStoreChange::REMOVE:
+        forms_to_remove.push_back(
+            std::make_unique<PasswordForm>(change.form()));
+        break;
+      default:
+        NOTREACHED();
+        break;
+    }
+  }
+
+  RemoveCredentials(std::move(forms_to_remove));
+
+  auto callback = base::BindOnce(
+      &CredentialProviderService::OnInjectedAffiliationAfterLoginsChanged,
+      weak_factory_.GetWeakPtr());
+
+  AffiliatedMatchHelper* matcher = password_store_->affiliated_match_helper();
+  if (matcher) {
+    matcher->InjectAffiliationAndBrandingInformation(
+        std::move(forms_to_add),
+        AndroidAffiliationService::StrategyOnCacheMiss::FETCH_OVER_NETWORK,
+        std::move(callback));
+  } else {
+    std::move(callback).Run(std::move(forms_to_add));
+  }
+}
+
+void CredentialProviderService::OnInjectedAffiliationAfterLoginsChanged(
+    std::vector<std::unique_ptr<PasswordForm>> forms) {
+  AddCredentials(std::move(forms));
+  SyncStore(false);
+}
+
+void CredentialProviderService::OnSyncConfigurationCompleted(
+    syncer::SyncService* sync) {
+  RequestSyncAllCredentialsIfNeeded();
+}
---
+++ src/ios/chrome/browser/ui/util/uikit_ui_util_unittest.mm
@@ -0,0 +1,129 @@
+// Copyright 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/util/uikit_ui_util.h"
+
+#include "base/ios/ios_util.h"
+#include "base/strings/sys_string_conversions.h"
+#import "ios/chrome/browser/ui/util/ui_util.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "testing/platform_test.h"
+#import "third_party/ocmock/OCMock/OCMock.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace {
+
+using UIKitUIUtilTest = PlatformTest;
+
+// Verify the assumption about UIViewController that on iPad and iOS 13 all
+// orientations are supported, and all orientations but Portrait Upside-Down on
+// iOS 12 iPhone and iPod Touch.
+TEST_F(UIKitUIUtilTest, UIViewControllerSupportedOrientationsTest) {
+  UIViewController* viewController =
+      [[UIViewController alloc] initWithNibName:nil bundle:nil];
+
+  if (IsIPadIdiom()) {
+    EXPECT_EQ(UIInterfaceOrientationMaskAll,
+              [viewController supportedInterfaceOrientations]);
+    return;
+  }
+
+  // Running on iPhone iOS 12 or earlier.
+  if (!base::ios::IsRunningOnIOS13OrLater()) {
+    EXPECT_EQ(UIInterfaceOrientationMaskAllButUpsideDown,
+              [viewController supportedInterfaceOrientations]);
+    return;
+  }
+
+  // Running on iOS 13 iPhone.
+
+  // Starting with iOS 13, the default [UIViewController
+  // supportedInterfaceOrientations] returns UIInterfaceOrientationMaskAll.
+  // However, this is only true if the application was built with the Xcode 11
+  // SDK (in order to preserve old behavior).
+  UIInterfaceOrientationMask expectedMask = UIInterfaceOrientationMaskAll;
+  EXPECT_EQ(expectedMask, [viewController supportedInterfaceOrientations]);
+}
+
+TEST_F(UIKitUIUtilTest, TestGetUiFont) {
+  EXPECT_TRUE(GetUIFont(FONT_HELVETICA, false, 15.0));
+  EXPECT_TRUE(GetUIFont(FONT_HELVETICA_NEUE, true, 15.0));
+}
+
+// Verifies that greyImage never returns retina-scale images.
+TEST_F(UIKitUIUtilTest, TestGreyImage) {
+  // Create an image using the device's scale factor.
+  const CGSize kSize = CGSizeMake(100, 100);
+  UIGraphicsBeginImageContextWithOptions(kSize, NO, 0.0);
+  UIImage* image = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+
+  // Verify the grey image's size and scale.
+  UIImage* greyImage = GreyImage(image);
+  EXPECT_EQ(kSize.width, greyImage.size.width);
+  EXPECT_EQ(kSize.height, greyImage.size.height);
+  EXPECT_EQ(1.0, greyImage.scale);
+}
+
+// Returns an image of random color in the same scale as the device main
+// screen.
+UIImage* testImage(CGSize imageSize) {
+  UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
+  CGContextRef context = UIGraphicsGetCurrentContext();
+  CGContextSetRGBStrokeColor(context, 0, 0, 0, 1.0);
+  CGContextSetRGBFillColor(context, 0, 0, 0, 1.0);
+  CGContextFillRect(context,
+                    CGRectMake(0.0, 0.0, imageSize.width, imageSize.height));
+  UIImage* image = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return image;
+}
+
+TEST_F(UIKitUIUtilTest, TestResizeImageOpacity) {
+  UIImage* actual;
+  UIImage* image = testImage(CGSizeMake(100, 100));
+  actual =
+      ResizeImage(image, CGSizeMake(50, 50), ProjectionMode::kAspectFit, YES);
+  EXPECT_TRUE(actual);
+
+  actual =
+      ResizeImage(image, CGSizeMake(50, 50), ProjectionMode::kAspectFit, NO);
+  EXPECT_TRUE(actual);
+}
+
+TEST_F(UIKitUIUtilTest, TestResizeImageInvalidInput) {
+  UIImage* actual;
+  UIImage* image = testImage(CGSizeMake(100, 50));
+  actual = ResizeImage(image, CGSizeZero, ProjectionMode::kAspectFit);
+  EXPECT_FALSE(actual);
+
+  actual = ResizeImage(image, CGSizeMake(0.1, 0.1), ProjectionMode::kAspectFit);
+  EXPECT_FALSE(actual);
+
+  actual =
+      ResizeImage(image, CGSizeMake(-100, -100), ProjectionMode::kAspectFit);
+  EXPECT_FALSE(actual);
+
+  actual = ResizeImage(nil, CGSizeMake(100, 100), ProjectionMode::kAspectFit);
+  EXPECT_FALSE(actual);
+}
+
+TEST_F(UIKitUIUtilTest, TintImageKeepsImageProperties) {
+  UIImage* image = testImage(CGSizeMake(100, 75));
+  UIImage* tintedImage = TintImage(image, [UIColor blueColor]);
+  EXPECT_EQ(image.size.width, tintedImage.size.width);
+  EXPECT_EQ(image.size.height, tintedImage.size.height);
+  EXPECT_EQ(image.scale, tintedImage.scale);
+  EXPECT_EQ(image.capInsets.top, tintedImage.capInsets.top);
+  EXPECT_EQ(image.capInsets.left, tintedImage.capInsets.left);
+  EXPECT_EQ(image.capInsets.bottom, tintedImage.capInsets.bottom);
+  EXPECT_EQ(image.capInsets.right, tintedImage.capInsets.right);
+  EXPECT_EQ(image.flipsForRightToLeftLayoutDirection,
+            tintedImage.flipsForRightToLeftLayoutDirection);
+}
+
+}  // namespace
