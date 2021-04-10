518c37f9bdacaeea46ad1fefab90ad7cd1a55b2d
edenda2 <edenda2@ac.sce.ac.il>
2021-04-10 04:37:26 +0300

    adding mutex for scedule threads for check exist password

---
+++ src/third_party/blink/renderer/core/layout/ng/table/layout_ng_table_cell.h
@@ -0,0 +1,151 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_TABLE_LAYOUT_NG_TABLE_CELL_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_TABLE_LAYOUT_NG_TABLE_CELL_H_
+
+#include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/html/table_constants.h"
+#include "third_party/blink/renderer/core/layout/layout_block_flow.h"
+#include "third_party/blink/renderer/core/layout/ng/layout_ng_block_flow_mixin.h"
+#include "third_party/blink/renderer/core/layout/ng/table/layout_ng_table_cell_interface.h"
+
+namespace blink {
+
+class LayoutNGTable;
+
+class CORE_EXPORT LayoutNGTableCell
+    : public LayoutNGBlockFlowMixin<LayoutBlockFlow>,
+      public LayoutNGTableCellInterface {
+ public:
+  explicit LayoutNGTableCell(Element*);
+
+  unsigned ComputedRowSpan() const {
+    if (!has_rowspan_)
+      return 1;
+    unsigned rowspan = ParseRowSpanFromDOM();
+    if (rowspan == 0)  // rowspan == 0 means all rows.
+      rowspan = kMaxRowSpan;
+    return rowspan;
+  }
+
+  const NGBoxStrut& IntrinsicLogicalWidthsBorderSizes() const {
+    return intrinsical_logical_widths_border_sizes_;
+  }
+
+  void SetIntrinsicLogicalWidthsBorderSizes(const NGBoxStrut& border_sizes) {
+    intrinsical_logical_widths_border_sizes_ = border_sizes;
+  }
+
+  // This method is called after a new *measure* layout-result is set.
+  // Tables are special in that the table accesses the table-cells directly
+  // for computing the size of the table-grid (bypassing the section/row).
+  // Due to this when we set a new measure layout-result, we also invalidate
+  // the layout-result cache of the associated section/row (as the cell
+  // fragment would be in the incorrect state).
+  void InvalidateLayoutResultCacheAfterMeasure() const;
+
+  LayoutNGTable* Table() const;
+
+  // LayoutBlockFlow methods start.
+
+  void UpdateBlockLayout(bool relayout_children) override;
+
+  void StyleDidChange(StyleDifference diff,
+                      const ComputedStyle* old_style) final;
+
+  // TODO(atotic) Remove "New" from name.
+  // Currently,  LayoutNGTableCellLegacy is named LayoutNGTableCell for test
+  // compat.
+  const char* GetName() const final { return "LayoutNGTableCellNew"; }
+
+  bool CreatesNewFormattingContext() const final { return true; }
+
+  LayoutBox* CreateAnonymousBoxWithSameTypeAs(
+      const LayoutObject* parent) const override;
+
+  // LayoutBlockFlow methods end.
+
+  // LayoutNGTableCellInterface methods start.
+
+  const LayoutTableCell* ToLayoutTableCell() const final {
+    NOTREACHED();
+    return nullptr;
+  }
+
+  const LayoutNGTableCellInterface* ToLayoutNGTableCellInterface() const final {
+    return this;
+  }
+  const LayoutObject* ToLayoutObject() const final { return this; }
+
+  LayoutObject* ToMutableLayoutObject() final { return this; }
+
+  LayoutNGTableInterface* TableInterface() const final;
+
+  void ColSpanOrRowSpanChanged() final;
+
+  Length StyleOrColLogicalWidth() const final;
+
+  // Not used in LayoutNG.
+  int IntrinsicPaddingBefore() const final { return 0; }
+  // Not used in LayoutNG.
+  int IntrinsicPaddingAfter() const final { return 0; }
+
+  unsigned RowIndex() const final;
+
+  unsigned ResolvedRowSpan() const final;
+
+  unsigned AbsoluteColumnIndex() const final;
+
+  // Guaranteed to be between kMinColSpan and kMaxColSpan.
+  unsigned ColSpan() const final;
+
+  LayoutNGTableCellInterface* NextCellInterface() const final;
+
+  LayoutNGTableCellInterface* PreviousCellInterface() const final;
+
+  LayoutNGTableRowInterface* RowInterface() const final;
+
+  LayoutNGTableSectionInterface* SectionInterface() const final;
+
+  // LayoutNGTableCellInterface methods end.
+
+ protected:
+  bool IsOfType(LayoutObjectType type) const final {
+    return type == kLayoutObjectTableCell ||
+           LayoutNGBlockFlowMixin<LayoutBlockFlow>::IsOfType(type);
+  }
+
+ private:
+  void UpdateColAndRowSpanFlags();
+
+  unsigned ParseRowSpanFromDOM() const;
+
+  unsigned ParseColSpanFromDOM() const;
+  // Use ComputedRowSpan instead
+  unsigned ParsedRowSpan() const {
+    if (!has_rowspan_)
+      return 1;
+    return ParseRowSpanFromDOM();
+  }
+
+  // Cached cell border. Used to invalidate calculation of
+  // intrinsic logical width.
+  NGBoxStrut intrinsical_logical_widths_border_sizes_;
+
+  unsigned has_col_span_ : 1;
+  unsigned has_rowspan_ : 1;
+};
+
+// wtf/casting.h helper.
+template <>
+struct DowncastTraits<LayoutNGTableCell> {
+  static bool AllowFrom(const LayoutObject& object) {
+    return object.IsTableCell() && !object.IsTableCellLegacy();
+  }
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_TABLE_LAYOUT_NG_TABLE_CELL_H_
---
+++ src/third_party/blink/renderer/modules/sanitizer_api/sanitizer.cc
@@ -0,0 +1,154 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "sanitizer.h"
+
+#include "third_party/blink/renderer/bindings/core/v8/v8_node_filter.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_sanitizer_config.h"
+#include "third_party/blink/renderer/core/dom/document_fragment.h"
+#include "third_party/blink/renderer/core/dom/element.h"
+#include "third_party/blink/renderer/core/dom/node.h"
+#include "third_party/blink/renderer/core/dom/node_traversal.h"
+#include "third_party/blink/renderer/core/editing/serializers/serialization.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/platform/bindings/exception_messages.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/bindings/script_state.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
+#include "third_party/blink/renderer/platform/wtf/vector.h"
+
+namespace blink {
+
+Sanitizer* Sanitizer::Create(const SanitizerConfig* config,
+                             ExceptionState& exception_state) {
+  return MakeGarbageCollected<Sanitizer>(config);
+}
+
+Sanitizer::Sanitizer(const SanitizerConfig* config)
+    : config_(const_cast<SanitizerConfig*>(config)) {
+  // Format dropElements to uppercase.
+  Vector<String> drop_elements = default_drop_elements_;
+  if (config->hasDropElements()) {
+    for (const String& s : config->dropElements()) {
+      if (!drop_elements.Contains(s.UpperASCII())) {
+        drop_elements.push_back(s.UpperASCII());
+      }
+    }
+  }
+  config_->setDropElements(drop_elements);
+
+  // Format allowElements to uppercase.
+  if (config->hasAllowElements()) {
+    Vector<String> l;
+    for (const String& s : config->allowElements()) {
+      if (!config_->dropElements().Contains(s))
+        l.push_back(s.UpperASCII());
+    }
+    config_->setAllowElements(l);
+  }
+
+  // Format dropAttributes to lowercase.
+  if (config->hasDropAttributes()) {
+    drop_attributes_ = default_drop_attributes_;
+    for (const String& s : config->dropAttributes()) {
+      drop_attributes_.push_back(WTF::AtomicString(s.LowerASCII()));
+    }
+  } else if (config->hasAllowAttributes()) {
+    Vector<String> l;
+    for (const String& s : config->allowAttributes()) {
+      if (!default_drop_attributes_.Contains(s))
+        l.push_back(s.LowerASCII());
+    }
+    config_->setAllowAttributes(l);
+  } else {
+    drop_attributes_ = default_drop_attributes_;
+  }
+}
+
+Sanitizer::~Sanitizer() = default;
+
+String Sanitizer::sanitizeToString(ScriptState* script_state,
+                                   const String& input,
+                                   ExceptionState& exception_state) {
+  return CreateMarkup(sanitize(script_state, input, exception_state),
+                      kChildrenOnly);
+}
+
+DocumentFragment* Sanitizer::sanitize(ScriptState* script_state,
+                                      const String& input,
+                                      ExceptionState& exception_state) {
+  LocalDOMWindow* window = LocalDOMWindow::From(script_state);
+  if (!window) {
+    exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
+                                      "Cannot find current DOM window.");
+    return nullptr;
+  }
+  Document* document = window->document();
+  DocumentFragment* fragment = document->createDocumentFragment();
+  DCHECK(document->QuerySelector("body"));
+  fragment->ParseHTML(input, document->QuerySelector("body"));
+
+  Node* node = fragment->firstChild();
+
+  while (node) {
+    // Skip non-Element nodes.
+    if (node->getNodeType() != Node::NodeType::kElementNode) {
+      node = NodeTraversal::Next(*node, fragment);
+      continue;
+    }
+
+    // TODO(crbug.com/1126936): Review the sanitising algorithm for non-HTMLs.
+    String node_name = node->nodeName().UpperASCII();
+    // If the current element is dropped, remove current element entirely and
+    // proceed to its next sibling.
+    if (config_->dropElements().Contains(node_name)) {
+      Node* tmp = node;
+      node = NodeTraversal::NextSkippingChildren(*node, fragment);
+      tmp->remove();
+    } else if (config_->hasAllowElements() &&
+               !config_->allowElements().Contains(node_name)) {
+      // If the current element is blocked, append its children after current
+      // node to parent node, remove current element and proceed to the next
+      // node.
+      Node* parent = node->parentNode();
+      Node* next_sibling = node->nextSibling();
+      while (node->hasChildren()) {
+        Node* n = node->firstChild();
+        if (next_sibling) {
+          parent->insertBefore(n, next_sibling, exception_state);
+        } else {
+          parent->appendChild(n, exception_state);
+        }
+        if (exception_state.HadException()) {
+          return nullptr;
+        }
+      }
+      Node* tmp = node;
+      node = NodeTraversal::Next(*node, fragment);
+      tmp->remove();
+    } else {
+      // Otherwise, remove any attributes to be dropped from the current
+      // element, and proceed to the next node (preorder, depth-first
+      // traversal).
+      Element* element = To<Element>(node);
+      for (const auto& name : element->getAttributeNames()) {
+        bool drop = drop_attributes_.Contains(name) ||
+                    (config_->hasAllowAttributes() &&
+                     !config_->allowAttributes().Contains(name));
+        if (drop)
+          element->removeAttribute(name);
+      }
+      node = NodeTraversal::Next(*node, fragment);
+    }
+  }
+
+  return fragment;
+}
+
+void Sanitizer::Trace(Visitor* visitor) const {
+  ScriptWrappable::Trace(visitor);
+  visitor->Trace(config_);
+}
+
+}  // namespace blink
